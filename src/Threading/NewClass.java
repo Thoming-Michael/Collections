22bd63972bb131fc80d4837d0f9a480cc556eae8
mthoming <mthoming@LIN-514720L.us.nelnet.biz>
2019-01-21 19:54:26 -0600

    First Commit with Threading Example

---
+++ src/Threading/NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Threading;
+
+import java.lang.*;
+
+/**
+ *
+ * @author mthoming
+ */
+public class NewClass implements Runnable {
+
+        Thread T;                        
+        public void run()                       
+        {                              
+                   
+        }
+    
+}
